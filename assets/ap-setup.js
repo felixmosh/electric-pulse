825e51fdcad29d7b88b67dd8f399f0e4064c47ac
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2023-10-23 22:52:47 +0300

    Progress with AP

---
+++ assets/ap-setup.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    console.log(form);
+  })
+})
